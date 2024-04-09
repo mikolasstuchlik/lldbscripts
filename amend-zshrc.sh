a15be387756303669862ae74dd9433937a795c9e
Mikoláš Stuchlík <stuchlej@gmail.com>
2024-04-09 10:39:54 +0200

    Add mising export

--- amend-zshrc.sh
+++ amend-zshrc.sh
@@ -6,5 +6,5 @@ ZSHRC_PATH="$HOME/.zshrc"
 
 echo "" >> $ZSHRC_PATH
 echo "# Add LLDB to PYTHON path https://lldb.llvm.org/use/python-reference.html#using-the-lldb-py-module-in-python" >> $ZSHRC_PATH
-echo "PYTHONPATH=\$PYTHONPATH:\$(lldb -P)" >> $ZSHRC_PATH
+echo "export PYTHONPATH=\$PYTHONPATH:\$(lldb -P)" >> $ZSHRC_PATH
 echo "" >> $ZSHRC_PATH
